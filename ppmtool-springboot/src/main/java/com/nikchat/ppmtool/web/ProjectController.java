b4474ca4e2d519f8c838886930f8514575f20e32
Nikolaos Chatzitheodorou <chatzitheodorou@intralot.com>
2021-03-05 15:05:43 +0200

    Project Object Validation (Error Responses from Server when passing Invalid Project object)

--- ppmtool-springboot/src/main/java/com/nikchat/ppmtool/web/ProjectController.java
+++ ppmtool-springboot/src/main/java/com/nikchat/ppmtool/web/ProjectController.java
@@ -1,8 +1,15 @@
 package com.nikchat.ppmtool.web;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +27,17 @@ public class ProjectController {
 
 
     @PostMapping("")
-    public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+    public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
+
+        if(result.hasErrors()){
+            Map<String, String> errorMap = new HashMap<>();
+
+            result.getFieldErrors().stream()
+            	.forEach(fieldError -> errorMap.put(fieldError.getField(), fieldError.getDefaultMessage()));
+            
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
+
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
