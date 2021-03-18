1ece708412d1aeda5e09129f18893cbdd440f666
Nikolaos Chatzitheodorou <chatzitheodorou@intralot.com>
2021-03-18 17:42:19 +0200

    Get Projects

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-import { GET_ERRORS } from "./types";
+import { GET_ERRORS, GET_PROJECTS } from "./types";
 
+/*** CREATE PROJECT ***/
 export const createProject = (project, history) => async (dispatch) => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
@@ -11,4 +12,14 @@ export const createProject = (project, history) => async (dispatch) => {
       payload: err.response.data,
     });
   }
+};
+
+/*** GET PROJECTS ***/
+export const getProjects = () => async (dispatch) => {
+  const res = await axios.get("http://localhost:8080/api/project/all");
+
+  dispatch({
+    type: GET_PROJECTS,
+    payload: res.data,
+  });
 };
