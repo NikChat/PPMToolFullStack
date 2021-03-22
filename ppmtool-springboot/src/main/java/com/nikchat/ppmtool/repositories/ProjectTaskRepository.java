acbb608cb11f6f79e86feb2b079bb8783dd22e9e
Nikolaos Chatzitheodorou <chatzitheodorou@intralot.com>
2021-03-22 12:58:14 +0200

    Backlog and ProjectTask entities

---
+++ ppmtool-springboot/src/main/java/com/nikchat/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package com.nikchat.ppmtool.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import com.nikchat.ppmtool.domain.ProjectTask;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
