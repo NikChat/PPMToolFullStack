77ff62eaef857044ee8b28ed4a33fa900692e90c
Nikolaos Chatzitheodorou <chatzitheodorou@intralot.com>
2021-04-06 12:38:14 +0300

    security action (Login) and reducer

--- ppmtool-react-client/src/reducers/index.js
+++ ppmtool-react-client/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import errorReducer from "./errorReducer";
 import projectReducer from "./projectReducer";
 import backlogReducer from "./backlogReducer";
+import securityReducer from "./securityReducer";
 
 // will take an object that contains all the reducers
 export default combineReducers({
   errors: errorReducer,
   project: projectReducer,
   backlog: backlogReducer,
+  security: securityReducer,
 });
